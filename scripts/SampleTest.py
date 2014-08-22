9d9518468171fb4e439b8f3fba99571b17c3d2a9
Whitney Yiu <wyiu@tnc.org>
2014-08-22 16:21:25 -0400

    Add some tests

--- scripts/SampleTest.py
+++ scripts/SampleTest.py
@@ -1,8 +1,3 @@
-"""
-===============
-Dashboard Goals
-===============
-"""
 import BaseTestCase
 
 from selenium import webdriver
@@ -15,6 +10,20 @@ class SampleTest(BaseTestCase.BaseTestCase):
     Load Google.com
     """
     @attr(tags=['deep', 'dashboard', 'google'])
-    def NatureAboutUs(self):
+    def GoogleLoad(self):
         driver = self.driver
         driver.get(self.base_url)
+
+    @attr(tags=['deep', 'dashboard', 'python'])
+    def PythonOrgBodyClass(self):
+        driver = self.driver
+        driver.get("Http://www.python.org")
+        bodyTag = driver.find_element_by_tag_name("body")
+        self.assertEqual("homepage", bodyTag.get_attribute("id"))
+    
+    @attr(tags=['deep', 'dashboard', 'python'])    
+    def FailedTest(self):
+        driver = self.driver
+        driver.get("Http://www.python.org")
+        bodyTag = driver.find_element_by_tag_name("body")
+        self.assertEqual("notarealid", bodyTag.get_attribute("id"))
